8c0467d9f808dba1d6b7337251da42b566f1fad3
Wéberson Oliveira <154702444+Webers0n@users.noreply.github.com>
2025-07-10 21:22:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const lista1 = ["teste", "iPhone 13", "Galaxy S21", "Redmi Note 12"]; // exemplo
 // EQUIPAMENTOS QUE ACEITA VOZ E DADOS
 const vozEDados = [
     // Xiaomi
-    "Redmi A3", "Redmi note 7", "Redmi note 8", "Redmi note 8 Pro", "Redmi 10C", "Redmi note 11",
-    "Redmi note 12", "Redmi 12C", "Redmi 13", "Redmi 13C", "Redmi 13 Pro", "Redmi Note 13 5G",
-    "Poco X3", "Poco X5", "Poco X5 Pro", "Poco X6 Pro", "Poco C65",
+    "Redmi note 7", "Redmi note 8", "Redmi note 8 Pro", "Redmi 10C", "Redmi note 11",
+    "Redmi note 12", "Redmi 13", "Redmi 13C", "Redmi 13 Pro", "Redmi Note 13 5G", "Redmi Note 12S",
+    "Poco X3", "Poco X5", "Poco X5 Pro", "Poco X6 Pro", "Poco C65", "Poco X7"," Poco X7 Pro",
 
     // Apple iPhone
     "iPhone 7", "iPhone 7 Plus", "iPhone 11", "iPhone 11 Pro", "iPhone 11 Pro Max",
@@ -16,12 +16,12 @@ const vozEDados = [
 
     // Samsung
     "Samsung A03", "Samsung A03 Core", "Samsung A04", "Samsung A04S", "Samsung A04E",
-    "Samsung A05", "Samsung A05S", "Samsung A12", "Samsung A13", "Samsung A14", "Samsung A15",
+    "Samsung A05", "Samsung A05S", "Samsung A12", "Samsung A13 5G", "Samsung A14", "Samsung A15",
     "Samsung A16 5G", "Samsung A23", "Samsung A32", "Samsung A34", "Samsung A35", "Samsung A53",
     "Samsung A54", // Galaxy A54 (pode usar só "Samsung A54")
-    "Samsung A71", "Samsung A72",
+    "Samsung A71",
     "Samsung M13", "Samsung M15", "Samsung M23", "Samsung M53", "Samsung M62",
-    "Samsung S23", "Samsung S23 Ultra",
+    "Samsung S23", "Samsung S23 Ultra","Samsung Galaxy S20 FE 5G",
 
     // Motorola
     "Moto E13", "Moto G14", "Moto G34", "Moto G53", "Moto G54", "Moto G60",
@@ -31,42 +31,42 @@ const vozEDados = [
 // EQUIPAMENTOS APENAS FUNCIONANDO OS DADOS
 const dadosApenas = [
     // LG
-    "LGK41", "LGK41S", "LGK52", "LGK62", "LGK22",
+    "LGK41", "LGK41S", "LGK52", "LGK62",
 
     // Motorola
-    "Moto E22", "Moto E40", "Moto G10", "Moto G20", "Moto G24", "Moto G32", "Moto G52", "M21S",
+    "Moto E22", "Moto E40", "Moto G10", "Moto G20", "Moto G24", "Moto G32", "Moto G52", "M21S","Moto One Macro","Moto G6",
 
     // Apple iPhone
     "iPhone 6S",
 
     // Samsung
-    "Samsung Galaxy S20 FE", "Samsung Pro", "Samsung A31", "Samsung Galaxy A50", "Samsung Galaxy A52", "Samsung Galaxy A20", "Samsung Galaxy A20S", "Samsung Galaxy A30S",
+    "Samsung Pro", "Samsung A31", "Samsung Galaxy A50", "Samsung Galaxy A52", "Samsung Galaxy A20", "Samsung Galaxy A20S", "Samsung Galaxy A30S","Samsung Galaxy S20 FE",
 
     // Realme
     "Realme C11", "Realme C33", "Realme C53", "Realme C55", "Realme C67", "Realme Note 50", "Realme C51", "Redmi Note 13",
 
     // Xiaomi
-    "Redmi 9A", "Redmi A2", "Redmi Note 9", "Redmi Note 9 Pro", "Redmi Note 9C", "Redmi 10A", "Redmi Note 10S", "Redmi Note 12S"
+    "Redmi 9A", "Redmi A2", "Redmi Note 9", "Redmi Note 9 Pro", "Redmi Note 9C", "Redmi 10A", "Redmi Note 10S"
 ];
 
 // Equipamentos imcompativeis
 const incompativeis = [
     // Samsung (incompatíveis)
     "Galaxy A01", "Samsung A10S", "Samsung S10", "Samsung A11",
-    "Samsung J7 Prime", "Samsung J7", "Samsung J6",
+    "Samsung J7 Prime", "Samsung J7", "Samsung J6","Samsung A13", "Samsung A72",
 
     // Motorola (incompatíveis)
-    "Moto One Macro", "Moto G8", "Moto G9 Plus", "Moto G22",
+    "Moto One Macro", "Moto G8", "Moto G9 Plus", "Moto G22","Moto G9",
 
     // Xiaomi
-    "Redmi note 6 Pro", "Mi A2", "Samsung Galaxy A31",
+    "Redmi note 6 Pro", "Mi A2", "Samsung Galaxy A31", "Redmi 12C","Redmi A3",
 
     // LG
-    "LG K9", "LG K10", "LG K12 MAX",
+    "LG K9", "LG K10", "LG K12 MAX","LG K22",
 
     // Outros
-    "A01", "A02S", "A03S", "A10", "J4", "J4 Core", "J7 Prime",
-    "G Max 2 SE 63", "Philco PCS02", "iPhone 8 Plus", " Samsung Galaxy A30"
+    "A02S", "A03S", "A10", "J4", "J4 Core", "J7 Prime",
+    "G Max 2 SE 63", "Philco PCS02", "iPhone 8 Plus", " Samsung Galaxy A30","Samsung Galaxy A31S",
 ];
 
 
